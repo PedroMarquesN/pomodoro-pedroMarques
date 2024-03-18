c9f46cc31e8ccc5e8b078453fae16212acb4452e
Pedro Marques <pedroleagueartmarques@gmail.com>
2024-03-18 20:27:32 -0300

    Feat: installing and init use React-hook-form

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -11,3 +11,4 @@ export const LayoutContainer = styled.div`
     display: flex;
     flex-direction: column;
 `
+
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -8,17 +8,29 @@ import {
     StartCountButton,
     TaskInput
 } from "./styles";
+import {useState} from "react";
+import {useForm} from "react-hook-form";
 
 export function Home() {
+    const {handleSubmit, register,watch} = useForm()
+
+    function handleCreateNewCycle(data:any) {
+        console.log(data)
+    }
+
+    const task = watch('task')
+    const isSubmitDisabled = !task
+
     return(
         <HomeContainer>
-            <form action="">
+            <form action="" onSubmit={handleSubmit(handleCreateNewCycle)}>
                 <FormContainer>
                     <label htmlFor="task">Vou trabalhar em</label>
                     <TaskInput
                         list="list-suggestion"
                         id="task"
                         placeholder="Dê um nome para seu projeto"
+                        {...register('task')}
                     />
 
                     <datalist id="list-suggestion">
@@ -34,7 +46,9 @@ export function Home() {
                         step={5}
                         min={5}
                         max={60}
-                        id="minutesAmount"/>
+                        id="minutesAmount"
+                        {...register('minutesAmount', {valueAsNumber: true})}
+                    />
 
                     <span>minutos.</span>
                 </FormContainer>
@@ -47,7 +61,7 @@ export function Home() {
                     <span>0</span>
                 </CountDown>
 
-                <StartCountButton disabled type="submit">
+                <StartCountButton disabled={isSubmitDisabled} type="submit">
                     <Play size={24}/>
                     Começar
                 </StartCountButton>
