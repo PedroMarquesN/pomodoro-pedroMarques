15b83ec8687c7e1fb035fc4b86908658f40dd1bb
Pedro Marques <pedroleagueartmarques@gmail.com>
2024-03-20 10:12:17 -0300

    Feat: finish formated date

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,6 +1,8 @@
 import {HistoryContainer, HistoryList, Status} from "./styles";
 import {useContext} from "react";
 import {CyclesContext} from "../../contexts/CyclesContext";
+import {formatDistanceToNow} from "date-fns";
+import {ptBR} from "date-fns/locale";
 
 export function History() {
     const {cycles} = useContext(CyclesContext)
@@ -27,7 +29,10 @@ export function History() {
                             <tr key={cycle.id}>
                                 <td>{cycle.task}</td>
                                 <td>{cycle.minutesAmount} minutos</td>
-                                <td>{cycle.startDate.toISOString()}</td>
+                                <td>{formatDistanceToNow(cycle.startDate, {
+                                    addSuffix: true,
+                                    locale: ptBR,
+                                })}</td>
                                 <td>
                                     {cycle.finishedDate && <Status statusColor="green">Concluído</Status>}
                                     {cycle.interruptDate && <Status statusColor="red">Interrompido</Status>}
